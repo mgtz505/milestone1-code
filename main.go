c09c1b2f0aeaa4fbbc39c8f6e75af9e8c0d33f5f
mgtz505 <gutierrez.marcusc@gmail.com>
2021-09-24 13:16:36 -0400

    Working on easier implementation

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+	"os"
 )
 
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Hellow World!</h1>"))
+}
+
 func main() {
-	fmt.Println("Test")
+	port := os.Getenv(("PORT"))
+	if port == "" {
+		port = "3000"
+	}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":"+port, mux)
 }
